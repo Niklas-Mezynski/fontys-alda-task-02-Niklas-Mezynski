83e55118f4eb19b6036165fb1446c532724d78e7
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-09-09 09:59:58 +0200

    Added test for construction of an AppointmentData

--- assignment/implementation/src/test/java/appointmentplanner/FactoryTest.java
+++ assignment/implementation/src/test/java/appointmentplanner/FactoryTest.java
@@ -1,11 +1,13 @@
 package appointmentplanner;
 
 import appointmentplanner.ServiceFinder;
-import appointmentplanner.api.AbstractAPFactory;
-import appointmentplanner.api.LocalDay;
-import appointmentplanner.api.LocalDayPlan;
+import appointmentplanner.api.*;
+
+import java.time.Duration;
 import java.time.LocalTime;
 import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -23,11 +25,21 @@ public class FactoryTest {
         fac = ServiceFinder.getFactory();
     }
 
+//    @Test
+//    void factoryCreatesDayPlan() {
+//        LocalDay day = LocalDay.now();
+//        LocalDayPlan ldp = fac.createLocalDayPlan( day, LocalTime.parse( "08:00" ), LocalTime.parse( "17:30" ) );
+//        assertThat( ldp ).as( fac.getClass().getName() + " should not return null object" ).isNotNull();
+//    }
+
     @Test
-    void factoryCreatesDayPlan() {
-        LocalDay day = LocalDay.now();
-        LocalDayPlan ldp = fac.createLocalDayPlan( day, LocalTime.parse( "08:00" ), LocalTime.parse( "17:30" ) );
-        assertThat( ldp ).as( fac.getClass().getName() + " should not return null object" ).isNotNull();
+    void t01createAppointmentData() {
+        AppointmentData appointment1 = fac.createAppointmentData("Doctor", Duration.ofHours(1));
+        AppointmentData appointment2 = fac.createAppointmentData("Doctor", Duration.ofHours(1), Priority.MEDIUM);
+        SoftAssertions.assertSoftly(s -> {
+            s.assertThat(appointment1).isNotNull();
+            s.assertThat(appointment2).isNotNull();
+        });
     }
 
 }
