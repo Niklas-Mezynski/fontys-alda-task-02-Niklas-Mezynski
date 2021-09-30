eb83310a477a225625be7d554380ffb35601e05f
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-09-30 10:10:51 +0200

    Added test for easy insertion of and appointment on a free timeline;

--- assignment/implementation/src/test/java/appointmentplanner/TimelineTest.java
+++ assignment/implementation/src/test/java/appointmentplanner/TimelineTest.java
@@ -1,9 +1,12 @@
 package appointmentplanner;
 
+import appointmentplanner.api.Appointment;
 import appointmentplanner.api.Timeline;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static appointmentplanner.TestData.TODAY;
 import static org.assertj.core.api.Assertions.*;
@@ -28,7 +31,13 @@ public class TimelineTest {
     }
 
     @Test
-    void t03addAppointmentFixedTime() {
+    void t03addAppointmentFixedTimeEasyCase() {
+        Optional<Appointment> appointment = timeline.addAppointment(TODAY, TestData.DATA3, TestData.T10_30);
+        SoftAssertions.assertSoftly(s -> {
+            s.assertThat(appointment.isEmpty()).isFalse();
+            s.assertThat(appointment.get().getStart()).isEqualTo(TestData.T10_30);
+            s.assertThat(appointment.get().getAppointmentData()).isSameAs(TestData.DATA3);
+        });
 
     }
 }
